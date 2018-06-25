f1310fa82617c63f5bfb7bea49c3eb3a32392608
Jonas Antunes <jonasbantunes@gmail.com>
2018-06-25 14:44:41 -0300

    Reformatação do código do projeto

--- src/comentarioAninhado.java
+++ src/comentarioAninhado.java
@@ -2,52 +2,40 @@ import compilador.lexer.Lexer;
 import compilador.lexer.LexerException;
 import compilador.node.*;
 
-public class comentarioAninhado extends Lexer
-{ 
-	private int count;
-	private TComentario comment;
-	private StringBuffer text;
-	
-	// Definimos o construtor
-	public comentarioAninhado(java.io.PushbackReader in)
-	{ 
-		super(in);
-	}
+public class comentarioAninhado extends Lexer {
+  private int count;
+  private TComentario comment;
+  private StringBuffer text;
 
-	// Definimos um filtro que reconhece comentarios aninhados
-	@Override
-	protected void filter() throws LexerException
-	{ 
-		//Coloque o código aqui...
-		{ // if we are in the comment state
-		    if(state.equals(State.COMENTARIO))
-		    { // if we are just entering this state
-		      if(comment == null)
-		      { // The token is supposed to be a comment.
-		        // We keep a reference to it and set the count to one
-		        comment = (TComentario) token;
-		        text = new StringBuffer(comment.getText());
-		        count = 1;
-		        token = null; // continue to scan the input.
-		      }
-		      else
-		      { // we were already in the comment state
-		        text.append(token.getText()); // accumulate the text.
-		        if(token instanceof TComentario)
-		          count++;
-		        else if(token instanceof TComentarioFim)
-		          count--;
-		        if(count != 0)
-		          token = null; // continue to scan the input.
-		        else
-		        { //comment.setText(text.toString());
-		          token = comment; //return a comment with the full text.
-		          state = State.NORMAL; //go back to normal.
-		          comment = null; // We release this reference.
-		        }
-		      }
-		    }
-		  }
-		
-	}
+  // Definimos o construtor
+  public comentarioAninhado(java.io.PushbackReader in) {
+    super(in);
+  }
+
+  // Definimos um filtro que reconhece comentarios aninhados
+  @Override
+  protected void filter() throws LexerException {
+    // Coloque o código aqui...
+    { // if we are in the comment state
+      if (state.equals(State.COMENTARIO)) { // if we are just entering this state
+        if (comment == null) { // The token is supposed to be a comment.
+          // We keep a reference to it and set the count to one
+          comment = (TComentario) token;
+          text = new StringBuffer(comment.getText());
+          count = 1;
+          token = null; // continue to scan the input.
+        } else { // we were already in the comment state
+          text.append(token.getText()); // accumulate the text.
+          if (token instanceof TComentario) count++;
+          else if (token instanceof TComentarioFim) count--;
+          if (count != 0) token = null; // continue to scan the input.
+          else { // comment.setText(text.toString());
+            token = comment; // return a comment with the full text.
+            state = State.NORMAL; // go back to normal.
+            comment = null; // We release this reference.
+          }
+        }
+      }
+    }
+  }
 }
