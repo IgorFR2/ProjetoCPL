84fd10bcc09a458a55f7146d7481ab0fe86b8540
Jonas Antunes <jonasbantunes@gmail.com>
2018-09-07 13:16:25 -0300

    Adicionado verificação de redeclaração de variável

--- src/Tradutor.java
+++ src/Tradutor.java
@@ -1,5 +1,27 @@
 import compilador.analysis.DepthFirstAdapter;
+import compilador.node.*;
+
+import java.util.Hashtable;
+import java.util.LinkedList;
 
 public class Tradutor extends DepthFirstAdapter {
+  Hashtable symbol_table = new Hashtable();
 
+  public void outANomeVariavel(ANomeVariavel node) {
+    String parent = node.parent().getClass().getSimpleName();
+    String key = node.getId().getText();
+    if (parent.equals("ADeclaracao")) {
+      if (symbol_table.containsKey(key)) {
+        System.out.println(
+            "["
+                + node.getId().getLine()
+                + ","
+                + node.getId().getPos()
+                + "] Redeclaração da variável "
+                + key);
+      } else {
+        symbol_table.put(key, key);
+      }
+    }
+  }
 }
